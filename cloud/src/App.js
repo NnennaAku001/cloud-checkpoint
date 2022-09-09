1cbcc0ae2c9a1cc8679cd250a21fbf1d9a2712dd
NnennaAku001 <nwenyinnenna92@gmail.com>
2022-09-09 19:16:44 +0100

    first commit

---
+++ cloud/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+import './App.css';
+
+function App( ) {
+  return (
+    <div className="App">
+      <iframe style={{ width:"100" , height:"500px"}}src="https://maps.google.com/maps?q=2880%20Broadway,%20New%20York&t=&z=13&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>
+     <h1>Hello World</h1>
+      <p>This is our landing page for cloud computing</p>
+     
+   </div>
+  )
+}
+
+export default App;
